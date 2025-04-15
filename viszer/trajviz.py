0fb4a830343174218616ba620a820e0f97e71596
RunqiuBao <runqiu.bao@tum.de>
2025-04-15 10:56:44 +0900

    support draw multiple trajs together

--- viszer/trajviz.py
+++ viszer/trajviz.py
@@ -1,6 +1,10 @@
 import open3d
 import numpy
+from typing import List
 from scipy.spatial.transform import Rotation as R
+
+import argparse
+import os
 
 
 def LoadTraj(format: str, pathToTraj: str, Tbias: numpy.ndarray=None):
@@ -35,12 +39,41 @@ def LoadTraj(format: str, pathToTraj: str, Tbias: numpy.ndarray=None):
     return trajPoses
 
 
-def DrawTraj(format: str, pathToTraj: str):
-    trajPoses = LoadTraj(format, pathToTraj)
-    positions = numpy.array([pose[:3, 3] for pose in trajPoses])
-    colors = numpy.zeros((len(trajPoses), 3))  # Random RGB colors
-    pcd = open3d.geometry.PointCloud()
-    pcd.points = open3d.utility.Vector3dVector(positions)
-    pcd.colors = open3d.utility.Vector3dVector(colors)
+def DrawTraj(format: str, listPathToTraj: List[str], listScaleFactors: List[int]):
+    listPcd = []
+    for pathToTraj, scaleFactor in zip(listPathToTraj, listScaleFactors):
+        trajPoses = LoadTraj(format, pathToTraj)
+        positions = numpy.array([pose[:3, 3] for pose in trajPoses]) * scaleFactor
+        colors = numpy.random.rand(1, 3)  # Random RGB colors
+        colors = numpy.tile(colors, (len(trajPoses), 1))
+        pcd = open3d.geometry.PointCloud()
+        pcd.points = open3d.utility.Vector3dVector(positions)
+        pcd.colors = open3d.utility.Vector3dVector(colors)
+        listPcd.append(pcd)
 
-    open3d.visualization.draw_geometries([pcd])
+    open3d.visualization.draw_geometries(listPcd)
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="show a trajectory.",
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        epilog=("Examples: \n" +
+                '%s --trajfile .../mydataset/aaa.txt \n' % os.path.basename(__file__)
+        )
+    )
+
+    parser.add_argument('--trajfile', '-i', nargs='+', action='store', type=str,
+                        help='Path to the trajectory file.')
+    parser.add_argument('--scale', '-s', nargs='+', action='store', type=float,
+                        help='scale factors for each trajfile.')
+    parser.add_argument('--trajformat', '-m', action='store', type=str, default="tum",
+                        help='Format of the traj. file. Default is %(default)s.')
+
+    args, remaining = parser.parse_known_args()
+
+    DrawTraj(args.trajformat, args.trajfile, args.scale)
+
+
+if __name__ == "__main__":
+    main()
